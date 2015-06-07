1e55f51a84c4ed2dbc5b024400908742ee570327
dmitriy <hector.tmb@gmail.com>
2015-06-07 12:54:37 +0300

    fix migrate

--- src/migrations/m150430_152401_base.php
+++ src/migrations/m150430_152401_base.php
@@ -16,10 +16,23 @@ class m150430_152401_base extends Migration
                 'display_in_config' => 1,
             ]
         );
+        $this->createTable(
+            '{{%object_twitter_card}}',
+            [
+                'id' => Schema::TYPE_PK,
+                'object_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+                'object_model_id' => Schema::TYPE_INTEGER .' NOT NULL',
+                'active' => Schema::TYPE_BOOLEAN . ' NOT NULL DEFAULT 1',
+                'title' => Schema::TYPE_STRING.' NOT NULL',
+                'description' => Schema::TYPE_TEXT.' NOT NULL',
+                'image' => Schema::TYPE_STRING
+            ]
+        );
     }
 
     public function down()
     {
         $this->delete('{{%configurable}}', ['module' => 'twitterCards']);
+        $this->delete('{{%object_twitter_card}}');
     }
 }
